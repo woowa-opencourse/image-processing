10333ec5df94ce18238b559f74b5a82df1857902
Gyueun <103631319+Gyueunn@users.noreply.github.com>
2025-11-06 15:09:12 +0900

    Initial project setup

---
+++ src/test/java/com/example/imageprocessing/ImageProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
