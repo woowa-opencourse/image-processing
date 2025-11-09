e041e8465866c4728ed93ecc096424911e5233e4
Gyueun <103631319+Gyueunn@users.noreply.github.com>
2025-11-09 21:34:43 +0900

    feat: 프론트엔드와 백엔드 간 이미지 흑백 변환 기능 연동

--- backend/src/main/java/com/example/imageprocessing/controller/ImageController.java
+++ backend/src/main/java/com/example/imageprocessing/controller/ImageController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
