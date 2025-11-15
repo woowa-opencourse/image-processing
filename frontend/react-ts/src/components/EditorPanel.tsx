8ca3d881ba337392072607f63a0dfdfcfce744a0
Gyueun <103631319+Gyueunn@users.noreply.github.com>
2025-11-15 20:45:13 +0900

    feat(crop): cropMode 시 Crop 버튼 색 변경 기능 추가

--- frontend/react-ts/src/components/EditorPanel.tsx
+++ frontend/react-ts/src/components/EditorPanel.tsx
@@ -1,23 +1,35 @@
 interface Props {
-    onFilter: (type: string) => void
+    onFilter: (type: string) => void;
+    cropMode: boolean;
 }
 
-const buttons = ['GrayScale', 'Brightness', 'Inversion', 'Crop', 'Reset']
+const buttons = ["GrayScale", "Brightness", "Inversion", "Crop", "Reset"];
 
-export default function EditorPanel({ onFilter }: Props) {
+export default function EditorPanel({ onFilter, cropMode }: Props) {
     return (
-        <div
-            className="w-full flex flex-wrap justify-center gap-4 p-4"
-        >
-            {buttons.map((button) => (
-                <button
-                    key={button}
-                    onClick={() => onFilter(button)}
-                    className="border-2 border-black px-4 py-1 font-semibold text-2xl hover:bg-gray-100 flex-1 mx-4 rounded-md"
-                >
-                    {button}
-                </button>
-            ))}
+        <div className="w-full flex flex-wrap justify-center gap-4 p-4">
+            {buttons.map((button) => {
+                const isCrop = button === "Crop";
+
+                return (
+                    <button
+                        key={button}
+                        onClick={() => onFilter(button)}
+                        className={`
+                            border-2 border-black px-4 py-1 font-semibold text-2xl 
+                            flex-1 mx-4 rounded-md transition
+                            ${isCrop
+                            ? (cropMode
+                                ? "bg-red-500 text-white"
+                                : "bg-gray-200 hover:bg-gray-300")
+                            : "bg-gray-200 hover:bg-gray-100"
+                        }
+                        `}
+                    >
+                        {button}
+                    </button>
+                );
+            })}
         </div>
-    )
+    );
 }
