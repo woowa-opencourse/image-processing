bd1ce8923af43a22c64ec0730b6dc1a9c5309a75
Gyueun <103631319+Gyueunn@users.noreply.github.com>
2025-11-15 20:45:13 +0900

    feat(crop): cropMode 시 Crop 버튼 색 변경 기능 추가

--- frontend/react-ts/src/components/ImageEditor.tsx
+++ frontend/react-ts/src/components/ImageEditor.tsx
@@ -133,7 +133,7 @@ export default function ImageEditor() {
 
     return (
         <div className="p-50 flex flex-col gap-y-8 md:gap-y-10 w-full max-w-[1200px] min-h-[80vh] mx-auto">
-            <EditorPanel onFilter={handleFilter} />
+            <EditorPanel onFilter={handleFilter} cropMode={isCropMode} />
             <div className="flex flex-col md:flex-row justify-center items-end gap-x-40 gap-y-10 p-8 w-full max-w-[1200px] mx-auto">
                 <ImageCanvas
                     image={image}
